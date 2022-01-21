31b4a2d1cc0ad028e688ad5a648801b2bfc12d25
crs <ornaldo.hasa@fshnstudent.info>
2022-01-21 14:52:17 +0100

    m

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _useReactIpDetails = _interopRequireDefault(require("./components/useReactIpDetails"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = _useReactIpDetails.default;
+exports.default = _default;
--- src/lib/components/useReactIpDetails.jsx
+++ src/lib/components/useReactIpDetails.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "preact/hooks";
+import { useState, useCallback, useEffect } from "preact/compat";
 import codeToCurrency from "./countryCodeToCurrency";
 import codeToLocal from "./countryCodesToLocal";
 
